27f2c2a3589db0bc0b43eb7f6619a14a41cda3da
Romulo <rhabbiati@hotmail.com>
2022-11-11 06:13:24 +0000

    Resource entity

---
+++ backend/src/main/java/com/smartgroup/smartlearn/entities/ResourceType.java
@@ -0,0 +1,7 @@
+package com.smartgroup.smartlearn.entities;
+
+public enum ResourceType {
+	
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+	
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('HTML Bootcamp', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-11-20T00:00:00Z', TIMESTAMP WITH TIME ZONE '2023-11-20T00:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-12-20T00:00:00Z', TIMESTAMP WITH TIME ZONE '2023-12-20T00:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('HTML Path', 'Course main path', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUx7nTmk9Ml-phMBdX4ocfUfU02OrDfK9pA&usqp=CAU', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Place to ask your queries', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUx7nTmk9Ml-phMBdX4ocfUfU02OrDfK9pA&usqp=CAU', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Exclusive lives to the class', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUx7nTmk9Ml-phMBdX4ocfUfU02OrDfK9pA&usqp=CAU', 0, 1);
