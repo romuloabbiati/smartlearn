4589b184c6b223d26e30842eb648633ec716210a
Romulo <rhabbiati@hotmail.com>
2022-11-15 04:53:38 +0000

    User by id

---
+++ backend/src/main/java/com/smartgroup/smartlearn/UserResource.java
@@ -0,0 +1,26 @@
+package com.smartgroup.smartlearn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartgroup.smartlearn.dto.UserDTO;
+import com.smartgroup.smartlearn.services.UserService;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		UserDTO userDTO = userService.findById(id);
+		return ResponseEntity.ok().body(userDTO);
+	}
+	
+}
--- backend/src/main/java/com/smartgroup/smartlearn/services/UserService.java
+++ backend/src/main/java/com/smartgroup/smartlearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.smartgroup.smartlearn.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.smartgroup.smartlearn.dto.UserDTO;
 import com.smartgroup.smartlearn.entities.User;
 import com.smartgroup.smartlearn.repositories.UserRepository;
+import com.smartgroup.smartlearn.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -18,6 +23,13 @@ public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> object = userRepository.findById(id);
+		User user = object.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
+		return new UserDTO(user);
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
